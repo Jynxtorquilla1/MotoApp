2b65e8167d15b17f20469ed9dffdb6bec35250af
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-12-19 18:11:39 +0100

    Add project files.

---
+++ MotoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+
+var employeeRepository = new GenericRepository<Employee>();
+employeeRepository.Add(new Employee { FirstName = "Kura" });
+employeeRepository.Add(new Employee { FirstName = "Kaczka" });
+employeeRepository.Add(new Employee { FirstName = "Gołąb" });
+employeeRepository.Save();
+
+
+
+
